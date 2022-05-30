2ae044ea373af27801590abdb4b402c1740c6f91
JuliansRecent <juliansrecent@gmail.com>
2022-05-30 12:34:56 -0700

    Add manual project slider/slideshow

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -15,9 +15,13 @@ const aboutMeButton = document.querySelector('.about-me-link');
 // Resume nav button
 const resumeButton = document.querySelector('.resume-link');
 
-// Projects page and it's nav button
+// Projects page, it's nav button, and button to change projects
 const projectsPage = document.querySelector('.projects-page');
 const projectsButton = document.querySelector('.projects-link');
+const previousButton = document.querySelector('.previous');
+const nextButton = document.querySelector('.next');
+const projects = document.querySelectorAll('.project-lis')
+
 
 // Contact page, it's nav button, send button, and form
 const contactPage = document.querySelector('.contact-page');
@@ -39,6 +43,7 @@ projectsButton.addEventListener('click', projectsPageAppear);
 contactButton.addEventListener('click', contactPageAppear);
 // SEND-BUTTON
 sendButton.addEventListener('click', clearForm);
+
 // Functions
 // Brings the nav page up and makes other pages hide
 function navPageAppear() {
@@ -72,6 +77,37 @@ function contactPageAppear() {
 	contactPage.classList.remove('hidden');
 	navPage.classList.add('hidden');
 }
+
+// Changes the project you are viewing
+let projectIndex = 1;
+showProjects(projectIndex);
+
+function nextProject(n) {
+	showProjects((projectIndex += n));
+}
+
+function currentProject(n) {
+	showProjects((projectIndex = n));
+}
+
+function showProjects(n) {
+    let i;
+	if (n > projects.length) {
+		projectIndex = 1;
+	}
+	if (n < 1) {
+		projectIndex = projects.length;
+	}
+    for (i = 0; i < projects.length; i++) {
+		projects[i].style.display = 'none';
+	}
+    projects[projectIndex - 1].style.display = 'flex';
+    
+}
+
+console.log(projectIndex)
+console.log(projects)
+
 
 // Resets the form after the submit button is clicked
 function clearForm() {
